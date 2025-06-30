eb8b7ed1c5a98a44cf43dc85e4fe7e92c0e85bda
mitrajunior <35549620+mitrajunior@users.noreply.github.com>
2025-06-30 12:51:31 +0100

    Fix angular build configuration

--- src/app/services/worker.service.spec.ts
+++ src/app/services/worker.service.spec.ts
@@ -1,16 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { WorkerService } from './worker.service';
+import { environment } from '../../environments/environment';
 
 describe('WorkerService', () => {
   let service: WorkerService;
+  let http: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [WorkerService]
+    });
     service = TestBed.inject(WorkerService);
+    http = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => http.verify());
+
+  it('should request group worker info', () => {
+    service.getGroupWorkerInfo('a', 'b').subscribe();
+    const req = http.expectOne(`${environment.API_URL}/api/client/a/b`);
+    expect(req.request.method).toBe('GET');
   });
 });
